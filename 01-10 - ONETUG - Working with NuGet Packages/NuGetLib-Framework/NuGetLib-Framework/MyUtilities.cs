d524f153591c3e2c1cfe6c657d0f78aab318d44d
Al Rodriguez <ProgrammerAl@outlook.com>
2019-01-06 20:00:03 -0500

    Added to ONETUG presentation and added example projects

---
+++ 01-10 - ONETUG - Working with NuGet Packages/NuGetLib-Framework/NuGetLib-Framework/MyUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetLib_Framework
+{
+    public class MyUtilities
+    {
+        public string LeftPad(string text, int requiredLength)
+        {
+            if (text.Length >= requiredLength)
+            {
+                return text;
+            }
+
+            var builder = new StringBuilder(requiredLength);
+            var spacesToAdd = text.Length - requiredLength;
+            builder.Append(' ', spacesToAdd);
+            builder.Append(text);
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ 01-10 - ONETUG - Working with NuGet Packages/NuGetLib-Standard/NuGetLib-Standard/MyUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace NuGetLib_Standard
+{
+    public class MyUtilities
+    {
+        public string LeftPad(string text, int requiredLength)
+        {
+            if (text.Length >= requiredLength)
+            {
+                return text;
+            }
+
+            var builder = new StringBuilder(requiredLength);
+            var spacesToAdd = text.Length - requiredLength;
+            builder.Append(' ', spacesToAdd);
+            builder.Append(text);
+
+            return builder.ToString();
+        }
+    }
+}
