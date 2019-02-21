8dd70099dc210c871b97d065eeef7c969de236f9
Al Rodriguez <ProgrammerAl@outlook.com>
2019-02-20 20:42:33 -0500

    Started demo code for TinyCLR OS demo app

---
+++ 03-30 - Orlando CC - .NET of Things/Demos/TinyCLR OS/TinyCLRApplicationSample/SharedNetStandard/IMyTimer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SharedNetStandard
+{
+    public interface IMyTimer
+    {
+    }
+}
---
+++ 03-30 - Orlando CC - .NET of Things/Demos/TinyCLR OS/TinyCLRApplicationSample/TinyCLRApplicationSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+using Utilities;
+using FezPins = GHIElectronics.TinyCLR.Pins.FEZ;
+
+namespace TinyCLRApplicationSample
+{
+    class Program
+    {
+        static void Main()
+        {
+            GpioPin led = GpioController.GetDefault().OpenPin(FezPins.GpioPin.D2);
+            led.SetDriveMode(GpioPinDriveMode.Output);
+
+            var sleepTimer = new SleepTimer(TimeSpan.FromMilliseconds(100));
+            var ledControl = new LedControl(led, sleepTimer);
+
+            while (true)
+            {
+                ledControl.Blink();
+            }
+        }
+    }
+}
