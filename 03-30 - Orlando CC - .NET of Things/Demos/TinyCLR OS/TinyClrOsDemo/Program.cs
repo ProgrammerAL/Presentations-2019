f728d650463ac9985433c9f07e52d240a3ffa5fd
Al Rodriguez <ProgrammerAl@outlook.com>
2019-02-28 20:24:55 -0500

    Added code for Windows 10 IoT Demo project. Small changes to TinyCLR OS code to match up with UWP.

--- 03-30 - Orlando CC - .NET of Things/Demos/TinyCLR OS/TinyClrOsDemo/Program.cs
+++ 03-30 - Orlando CC - .NET of Things/Demos/TinyCLR OS/TinyClrOsDemo/Program.cs
@@ -3,8 +3,7 @@ using System.Threading;
 using GHIElectronics.TinyCLR.Devices.Gpio;
 using GHIElectronics.TinyCLR.Devices.I2c;
 using GHIElectronics.TinyCLR.Devices.I2c.Provider;
-using HardwareDrivers;
-using HardwareDrivers.LightSensor.APDS9301;
+using HardwareDrivers.LightSensors.APDS9301;
 using Utilities;
 using FezPins = GHIElectronics.TinyCLR.Pins.FEZ;
 
@@ -29,8 +28,8 @@ namespace TinyCLRApplicationSample
 
             int sdaPin = FezPins.GpioPin.A0;
             int slcPin = FezPins.GpioPin.A1;
-            int ledDeviceAddress = 0x39;
-            I2cConnectionSettings ledDeviceConnectionSettings = new I2cConnectionSettings(ledDeviceAddress, I2cAddressFormat.SevenBit, I2cBusSpeed.StandardMode);
+            int lightSensorDeviceAddress = 0x39;
+            I2cConnectionSettings ledDeviceConnectionSettings = new I2cConnectionSettings(lightSensorDeviceAddress, I2cAddressFormat.SevenBit, I2cBusSpeed.StandardMode);
 
             I2cControllerSoftwareProvider i2cProvider = new I2cControllerSoftwareProvider(sdaPin, slcPin, false);
             I2cController i2cController = I2cController.FromProvider(i2cProvider);
